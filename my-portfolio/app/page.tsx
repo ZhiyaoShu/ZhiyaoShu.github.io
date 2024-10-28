d9490fd19d01ebda4a22c7d50021ae29e2e85823
Zoey Shu <yaoshu0326@berkeley.edu>
2024-10-27 22:08:06 -0700

    Update page.tsx: Add PhD research opportunity message

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -82,6 +82,8 @@ export default function Page() {
           classification. Committed to developing innovative solutions and
           pushing the boundaries of cutting-edge technology.
         </p>
+        <br />
+        I am actively looking for PhD research opportunities. Please feel free to reach out to me if you think my research aligns with your interests.
         <div className="my-8"></div>
       </section>
 
