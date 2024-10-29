22032ef4894b1fbf2a821922c0bc2235bab0aa16
Zoey Shu <yaoshu0326@berkeley.edu>
2024-10-28 19:13:19 -0700

    Update page.tsx: Update career description and add research engineer opportunity

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -82,7 +82,7 @@ export default function Page() {
           classification. Committed to developing innovative solutions and
           pushing the boundaries of cutting-edge technology.
         </p>
-        <br />I am actively looking for PhD research opportunities. Please feel
+        <br />I am actively looking for PhD or research engineer opportunities. Please feel
         free to reach out to me if you think my research aligns with your
         interests.
         <div className="my-8"></div>
